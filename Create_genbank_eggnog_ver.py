82cad7ca86c38b68997e1d49e903bc43f8a7ce01
Baptiste Ruiz <63844511+baptisteruiz@users.noreply.github.com>
2020-04-29 16:12:43 +0200

    Update Create_genbank_eggnog_ver.py

--- Create_genbank_eggnog_ver.py
+++ Create_genbank_eggnog_ver.py
@@ -8,6 +8,7 @@ Created on Tue Apr 21 11:16:43 2020
 
 
 import os
+import requests
 
 from Bio import SeqIO
 from Bio import SeqFeature as sf
@@ -23,12 +24,46 @@ cloudfile = "fasta_gene_annotation/cloud_seq_methanosarcina_complete/cloud_seq_m
 persistentfile = "fasta_gene_annotation/persistent_seq_methanosarcina_complete/persistent_seq_methanosarcina_complete.emapper.annotations"          #fichier eggnog annotant la partition persistent
 shellfile = "fasta_gene_annotation/shell_seq_methanosarcina_complete/shell_seq_methanosarcina_complete.emapper.annotations"                         #fichier eggnog annotant la partition shell
 allgenesfile = "all_genes/all_genes.fna"                                                                                                            #fichier de sortie de la commande write --all genes de PPanGGOLiN
+infoprot= "all_prot_families/representative_gene_families.faa"                                                                                      #fichier de sortie de la commande PPanGGOLiN write --all_prot_families
 
+#Définir ici le nom de l'espèce étudiée
+speciesname = "Methanosarcina"
 
 
 ##FONCTION
 
-def Eggnog_Genbank(cloud_file, persistent_file, shell_file, all_genes_file)
+def create_taxonomic_data(species_name):
+    """
+    Query the EBI with the species name to create a dictionary containing taxon id,
+    taxonomy and some other informations.
+    """
+    species_informations = {}
+    species_name_url = species_name.replace(' ', '%20')
+
+    url = 'https://www.ebi.ac.uk/ena/data/taxonomy/v1/taxon/scientific-name/' + species_name_url
+    response = requests.get(url)
+    temp_species_informations = response.json()[0]
+
+    for temp_species_information in temp_species_informations:
+        if temp_species_information == 'lineage':
+            species_informations['taxonomy'] = temp_species_informations[temp_species_information].split('; ')[:-1]
+        elif temp_species_information == 'division':
+            species_informations['data_file_division'] = temp_species_informations[temp_species_information]
+        elif temp_species_information == 'taxId':
+            species_informations['db_xref'] = 'taxon:' + str(temp_species_informations[temp_species_information])
+        else:
+            species_informations[temp_species_information] = temp_species_informations[temp_species_information]
+
+    compatible_species_name = species_name.replace('/', '_')
+    species_informations['description'] = compatible_species_name + ' genome'
+    species_informations['organism'] = compatible_species_name
+    species_informations['keywords'] = [compatible_species_name]
+
+    return species_informations
+
+
+
+def Eggnog_Genbank(cloud_file, persistent_file, shell_file, all_genes_file, info_prot, species_name)
 
     #On classe les séquences listées dans le fichier fasta en 3 strings, selon qu'on retrouve leur identifiant dans le fichier cloud, persistent ou shell
 
@@ -66,13 +101,27 @@ def Eggnog_Genbank(cloud_file, persistent_file, shell_file, all_genes_file)
     cloud_len = 0
     persistent_len = 0
     shell_len = 0
+    
+    #On importe les séquences protéiques correspondant aux traductions dans un dictionaire
+        
+    dictionaire_trad = {}
+
+    info_prot_open = open(info_prot, 'r')
+    read_info_prot = [mylines for mylines in info_prot_open.readlines()]
+
+
+    for i in range(0, len(read_info_prot), 2):
+        nom_gene = (read_info_prot[i].replace(">","")).replace("\n", "")
+        dictionaire_trad[nom_gene] = read_info_prot[i+1].replace("\n", "")
+        
+        
 
     for seq_record in SeqIO.parse(all_genes_file, "fasta"):
         trouve = False
         for idec in cloud_ecidlist:
             if (seq_record.id in idec):
                 cloudstring+=str(seq_record.seq)
-                cloud_eclist.append([cloud_len, cloud_len + len(str(seq_record.seq)), idec[1]])
+                cloud_eclist.append([cloud_len, cloud_len + len(str(seq_record.seq)), idec[1], dictionaire_trad[idec[0]]])
                 cloud_len += len(str(seq_record.seq))
                 trouve = True
             
@@ -80,7 +129,7 @@ def Eggnog_Genbank(cloud_file, persistent_file, shell_file, all_genes_file)
             for idec in shell_ecidlist:
                 if (seq_record.id in idec):
                     shellstring+=str(seq_record.seq)
-                    shell_eclist.append([shell_len, shell_len + len(str(seq_record.seq)), idec[1]])
+                    shell_eclist.append([shell_len, shell_len + len(str(seq_record.seq)), idec[1], dictionaire_trad[idec[0]]])
                     shell_len += len(str(seq_record.seq))
                     trouve = True
                 
@@ -88,7 +137,7 @@ def Eggnog_Genbank(cloud_file, persistent_file, shell_file, all_genes_file)
                 for idec in persistent_ecidlist:
                     if (seq_record.id in idec):
                         persistentstring+=str(seq_record.seq)
-                        persistent_eclist.append([persistent_len, persistent_len + len(str(seq_record.seq)), idec[1]])
+                        persistent_eclist.append([persistent_len, persistent_len + len(str(seq_record.seq)), idec[1], dictionaire_trad[idec[0]]])
                         persistent_len += len(str(seq_record.seq))
                         trouve = True
 
@@ -104,30 +153,52 @@ def Eggnog_Genbank(cloud_file, persistent_file, shell_file, all_genes_file)
     persistent_record = SeqRecord(persistentlist, id="Methanosarcina_Persistent_Pseudocontig", name="persistent", description="Pseudo_contig for the methanosarcina persistent genome")
     shell_record = SeqRecord(shelllist, id="Methanosarcina_Shell_Pseudocontig", name="shell", description="Pseudo_contig for the methanosarcina shell genome")
 
+    
     #On crée les Features contenant les EC correspondant à chaque partition, puis on les ajoute aux SeqRecords
-
 
             
     for i in cloud_eclist:
         cloud_feature_cds = sf.SeqFeature(sf.FeatureLocation(i[0], i[1], +1), type = 'CDS')
-        if i[2] != '':
-            eclist = i[2].split(',')
-            cloud_feature_cds.qualifiers['EC_number'] = eclist
+        if i[2] != []:
+            cloud_feature_cds.qualifiers['EC_number'] = [(ec.replace('ec:', '')).replace('\n','') for ec in i[2]]
+        cloud_feature_cds.qualifiers['locus_tag'] = i[1]
+        cloud_feature_cds.qualifiers['translation'] = i[3]
         cloud_record.features.append(cloud_feature_cds)
 
     for i in persistent_eclist:
         persistent_feature_cds = sf.SeqFeature(sf.FeatureLocation(i[0], i[1], +1) , type = 'CDS')
-        if i[2] != '':
-            eclist = i[2].split(',')
-            persistent_feature_cds.qualifiers['EC_number'] = eclist
+        if i[2] != []:
+            persistent_feature_cds.qualifiers['EC_number'] = [(ec.replace('ec:', '')).replace('\n','') for ec in i[2]]
+        persistent_feature_cds.qualifiers['locus_tag'] = i[1]
+        persistent_feature_cds.qualifiers['translation'] = i[3]
         persistent_record.features.append(persistent_feature_cds)
         
     for i in shell_eclist:
         shell_feature_cds = sf.SeqFeature(sf.FeatureLocation(i[0], i[1], +1), type = 'CDS')
-        if i[2] != '':
-            eclist = i[2].split(',')
-            shell_feature_cds.qualifiers['EC_number'] = eclist
+        if i[2] != []:
+            shell_feature_cds.qualifiers['EC_number'] = [(ec.replace('ec:', '')).replace('\n','') for ec in i[2]]
+        shell_feature_cds.qualifiers['locus_tag'] = i[1]
+        shell_feature_cds.qualifiers['translation'] = i[3]
         shell_record.features.append(shell_feature_cds)
+        
+   #On crée les features information
+
+
+    species_information = create_taxonomic_data(species_name)
+
+    new_feature_source_cloud = sf.SeqFeature(sf.FeatureLocation(1-1, len(cloudstring)),type="source")
+    new_feature_source_shell = sf.SeqFeature(sf.FeatureLocation(1-1, len(shellstring)),type="source")
+    new_feature_source_persistent = sf.SeqFeature(sf.FeatureLocation(1-1, len(persistentstring)),type="source")
+
+
+    new_feature_source_cloud.qualifiers['db_xref'] = species_information['db_xref']
+    new_feature_source_shell.qualifiers['db_xref'] = species_information['db_xref']
+    new_feature_source_persistent.qualifiers['db_xref'] = species_information['db_xref']
+
+
+    cloud_record.features.append(new_feature_source_cloud)
+    shell_record.features.append(new_feature_source_shell)
+    persistent_record.features.append(new_feature_source_persistent)
         
 
 
@@ -144,5 +215,5 @@ def Eggnog_Genbank(cloud_file, persistent_file, shell_file, all_genes_file)
     
 ## SCRIPT
 
-Eggnog_Genbank(cloudfile, persistentfile, shellfile, allgenesfile)
+Eggnog_Genbank(cloudfile, persistentfile, shellfile, allgenesfile, infoprot, speciesname)
 
