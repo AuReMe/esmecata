3421f9f33280c2f6eb10637952a2402ce2a2782a
Arnaud Belcour <arnaud.belcour@inria.fr>
2021-07-09 10:19:42 +0200

    Add new version.

---
+++ esmecata/coreproteome.py
@@ -0,0 +1,79 @@
+import os
+import csv
+import subprocess
+
+from Bio import SeqIO
+
+# Use the result folder created by retrieve_proteome.py.
+result_folder = 'result'
+cluster_fasta_files = {}
+for cluster in os.listdir(result_folder):
+    result_cluster_folder = os.path.join(result_folder, cluster)
+    for cluster_file in os.listdir(result_cluster_folder):
+        filename, file_extension = os.path.splitext(cluster_file)
+        if file_extension == '.faa':
+            cluster_file_path = os.path.join(result_cluster_folder, cluster_file)
+            if cluster not in cluster_fasta_files:
+                cluster_fasta_files[cluster] = [cluster_file_path]
+            else:
+                cluster_fasta_files[cluster].append(cluster_file_path)
+
+# Create tmp folder for mmseqs analysis.
+coreproteome_tmp = 'coreproteome_tmp'
+if not os.path.exists(coreproteome_tmp):
+    os.mkdir(coreproteome_tmp)
+
+# Create output folder containing shared representative proteins.
+coreproteome = 'coreproteome'
+if not os.path.exists(coreproteome):
+    os.mkdir(coreproteome)
+
+print('Creating coreproteome')
+# For each OTU run mmseqs easy-cluster on them to found the clusters that have a protein in each proteome of the OTU. 
+# We take the representative protein of a cluster if the cluster contains a protein from all the proteomes of the OTU.
+# If this condition is not satisfied the cluster will be ignored.
+# Then a fasta file containing all the representative proteins for each OTU is written in coreproteome folder.
+for cluster in cluster_fasta_files:
+    coreproteome_tmp_cluster = os.path.join(coreproteome_tmp, cluster)
+    coreproteome_tmp_cluster_output = os.path.join(coreproteome_tmp_cluster, 'cluster')
+
+    # Run mmmseqs to find rapidly protein clusters.
+    if not os.path.exists(coreproteome_tmp_cluster):
+        os.mkdir(coreproteome_tmp_cluster)
+    subprocess.call(['mmseqs', 'easy-cluster', *cluster_fasta_files[cluster], coreproteome_tmp_cluster_output, coreproteome_tmp_cluster])
+
+    # Extract protein clusters.
+    proteins_representatives = {}
+    with open(coreproteome_tmp_cluster_output+'_cluster.tsv') as input_file:
+        csvreader = csv.reader(input_file, delimiter='\t')
+        for row in csvreader:
+            if row[0] not in proteins_representatives:
+                proteins_representatives[row[0]] = [row[1]]
+            else:
+                proteins_representatives[row[0]].append(row[1])
+
+    # Retrieve protein ID and the corresponding proteome.
+    organism_prots = {}
+    for fasta_file in cluster_fasta_files[cluster]:
+        filename, file_extension = os.path.splitext(fasta_file)
+        for record in SeqIO.parse(fasta_file, 'fasta'):
+            organism_prots[record.id.split('|')[1]] = fasta_file
+
+    # To keep a cluster, we have to find have at least one protein of each proteome of the OTU.
+    rep_prot_to_keeps = []
+    rep_prot_organims = {}
+    for rep_protein in proteins_representatives:
+        if len(proteins_representatives[rep_protein]) > 1:
+                rep_prot_organims[rep_protein] = set([organism_prots[prot] for prot in proteins_representatives[rep_protein]])
+                if len(rep_prot_organims[rep_protein]) == len(cluster_fasta_files[cluster]):
+                    rep_prot_to_keeps.append(rep_protein)
+
+    # Create BioPtyhon records with the representative proteins kept.
+    new_records = []
+    for record in SeqIO.parse(coreproteome_tmp_cluster_output + '_rep_seq.fasta', 'fasta'):
+        if record.id.split('|')[1] in rep_prot_to_keeps:
+            new_records.append(record)
+
+    # Create output proteome file for OTU.
+    coreproteome_fasta_fifle = os.path.join(coreproteome, cluster+'.faa')
+    SeqIO.write(new_records, coreproteome_fasta_fifle, 'fasta')
---
+++ esmecata/retrieve_proteome.py
@@ -0,0 +1,123 @@
+import pandas as pd
+import time
+import requests
+import os
+import csv
+import shutil
+import gzip
+
+from ete3 import NCBITaxa
+
+df = pd.read_excel('Example.xlsx')
+
+# Set index on the column containing the OTU name.
+df.set_index('observation_name', inplace=True)
+
+# taxonomy is the column containing the taxonomy separated by ';': phylum;class;order;family;genus;genus + species
+taxonomies = df.to_dict()['taxonomy']
+
+ncbi = NCBITaxa()
+tax_ids = []
+
+tax_id_names = {}
+cluster_ids = {}
+clusters_dicts = {}
+
+# For each taxonomy find the taxonomy ID corresponding to each taxon.
+for cluster in taxonomies:
+    if isinstance(taxonomies[cluster], str):
+        taxons = [taxon for taxon in taxonomies[cluster].split(';')]
+        names = ncbi.get_name_translator(taxons)
+        invert_names = {v: k for k, vs in names.items() for v in vs }
+        tax_id_names.update(invert_names)
+        for names_tax_ids in list(names.values()):
+            tax_ids.extend(names_tax_ids)
+        cluster_ids[cluster] = names
+        clusters_dicts[cluster] = [names[tax_name] for tax_name in taxonomies[cluster].split(';') if tax_name in names]
+
+# Query the Uniprot proteomes to find all the proteome IDs associated to taxonomy.
+# If there is more thant 100 proteomes we do not keep it because there is too many proteome.
+print('Find proteome ID associated to taxonomy')
+tax_ids = set(tax_ids)
+proteomes_ids = {}
+for tax_id in tax_ids:
+    http_str = 'https://www.uniprot.org/proteomes/?query=reference:yes+taxonomy:{0}&format=list'.format(tax_id)
+
+    response = requests.get(http_str)
+    proteomes = response.text.split('\n')
+    proteomes.remove('')
+
+    if len(proteomes) > 0 and len(proteomes) < 100:
+        proteomes_ids[tax_id] = proteomes
+    time.sleep(1)
+
+# Write for each taxon ID the proteomes found.
+with open('proteome_tax_id.tsv', 'w') as out_file:
+    csvwriter = csv.writer(out_file, delimiter='\t')
+    csvwriter.writerow(['tax_id', 'name' ,'proteomes'])
+    for tax_id in proteomes_ids:
+        csvwriter.writerow([tax_id , tax_id_names[tax_id], ','.join(proteomes_ids[tax_id])])
+
+proteomes_ids = {}
+with open('proteome_tax_id.tsv', 'r') as out_file:
+    csvwriter = csv.reader(out_file, delimiter='\t')
+    for row in csvwriter:
+        proteomes_ids[row[0]] = row[2].split(',')
+
+# Retrieve the proteome to download. Meaning the proteome for the basal taxon in the taxonomy associated with Uniprot proteome.
+proteome_to_download = []
+cluster_proteomes = {}
+cluster_tax_ids = {}
+for cluster in clusters_dicts:
+    cluster_tax_ids[cluster] = [str(tax_id) for tax_id in clusters_dicts[cluster][-1]]
+    for tax_id in clusters_dicts[cluster][-1]:
+        if str(tax_id) in proteomes_ids:
+            tax_id_proteomes = proteomes_ids[str(tax_id)]
+            if cluster not in cluster_proteomes:
+                cluster_proteomes[cluster] = tax_id_proteomes
+            else:
+                cluster_proteomes[cluster].extend(tax_id_proteomes)
+            proteome_to_download.extend(tax_id_proteomes)
+proteome_to_download = set(proteome_to_download)
+
+# Write for each taxon the corresponding tax ID, the name of the taxon and the proteome associated with them.
+with open('proteome_cluster_tax_id.tsv', 'w') as out_file:
+    csvwriter = csv.writer(out_file, delimiter='\t')
+    csvwriter.writerow(['cluster', 'tax_id', 'name', 'proteome'])
+    for cluster in cluster_tax_ids:
+        cluster_tax_id_names = [tax_id_names[int(tax_id)] for tax_id in cluster_tax_ids[cluster]]
+        if cluster in cluster_proteomes:
+            cluster_tax_id_proteomes = [proteome for proteome in cluster_proteomes[cluster]]
+        else:
+            cluster_tax_id_proteomes = []
+        csvwriter.writerow([cluster , ','.join(cluster_tax_ids[cluster]), ','.join(cluster_tax_id_names), ','.join(cluster_tax_id_proteomes)])
+
+# Download all the proteomes in tmp folder.
+print('Downlaod proteome')
+output_folder = 'tmp_proteome'
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+for proteome in proteome_to_download:
+    output_proteome_file = os.path.join(output_folder, proteome+'.faa.gz')
+    proteome_response = requests.get('https://www.uniprot.org/uniprot/?query=proteome:{0}&format=fasta&compress=yes'.format(proteome))
+    with open(output_proteome_file, 'wb') as f:
+        f.write(proteome_response.content)
+    time.sleep(1)
+
+print('Creating result folder')
+# Create a result folder which contains one sub-folder per OTU.
+# Each OTU sub-folder will contain the proteome found.
+output_folder = 'result'
+if not os.path.exists(output_folder):
+    os.mkdir(output_folder)
+tmp_folder = 'tmp_proteome'
+for cluster in cluster_proteomes:
+    output_cluster = os.path.join(output_folder, cluster)
+    if not os.path.exists(output_cluster):
+        os.mkdir(output_cluster)
+    for proteome in cluster_proteomes[cluster]:
+        output_proteome = os.path.join(output_cluster, proteome+'.faa')
+        input_proteome_file = os.path.join(tmp_folder, proteome+'.faa.gz')
+        with gzip.open(input_proteome_file, 'rb') as f_in:
+            with open(output_proteome, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
