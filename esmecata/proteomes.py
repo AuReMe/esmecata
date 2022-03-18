236a374f8b28b746f51cd5ad78eae7de01dfb19f
Arnaud Belcour <arnaud.belcour@irisa.fr>
2022-03-18 12:03:36 +0100

    Fix wrong year.

--- esmecata/proteomes.py
+++ esmecata/proteomes.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2022 Arnaud Belcour - Inria Dyliss
+# Copyright (C) 2021-2022 Arnaud Belcour - Inria Dyliss
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
