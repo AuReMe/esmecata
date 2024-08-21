0e38a55d544d7610ee2169fe36e56598d5f63afa
Arnaud Belcour <arnaud.belcour@gmail.com>
2024-08-21 15:57:34 +0200

    Remove merge_fasta argument in workflow function.

--- esmecata/core/workflow.py
+++ esmecata/core/workflow.py
@@ -170,8 +170,7 @@ def perform_workflow_eggnog(input_file, output_folder, eggnog_database_path, bus
                             remove_tmp=None, limit_maximal_number_proteomes=99, rank_limit=None,
                             nb_core=1, clust_threshold=0.5, mmseqs_options=None,
                             linclust=None, minimal_number_proteomes=5, update_affiliations=None,
-                            option_bioservices=None, eggnog_tmp_dir=None, no_dbmem=False,
-                            merge_fasta=False):
+                            option_bioservices=None, eggnog_tmp_dir=None, no_dbmem=False):
     """From the proteomes found by esmecata proteomes, create protein cluster for each taxonomic affiliations.
 
     Args:
@@ -194,7 +193,6 @@ def perform_workflow_eggnog(input_file, output_folder, eggnog_database_path, bus
         option_bioservices (bool): use bioservices instead of manual queries.
         eggnog_tmp_dir (str): pathname to eggnog-mapper temporary folder.
         no_dbmem (bool): Boolean to choose to not load eggnog database in memory.
-        merge_fasta (bool): Merge fasta into bigger one (associated with superkingdom) to launch eggnog-mapper on few fasta files.
     """
     starttime = time.time()
     logger.info('|EsMeCaTa|workflow| Begin workflow.')
@@ -213,7 +211,7 @@ def perform_workflow_eggnog(input_file, output_folder, eggnog_database_path, bus
     make_clustering(proteomes_output_folder, clustering_output_folder, nb_core, clust_threshold, mmseqs_options, linclust, remove_tmp)
 
     annotation_output_folder = os.path.join(output_folder, '2_annotation')
-    annotate_with_eggnog(clustering_output_folder, annotation_output_folder, eggnog_database_path, nb_core, eggnog_tmp_dir, no_dbmem, merge_fasta)
+    annotate_with_eggnog(clustering_output_folder, annotation_output_folder, eggnog_database_path, nb_core, eggnog_tmp_dir, no_dbmem)
 
     stat_file = os.path.join(output_folder, 'stat_number_workflow.tsv')
     compute_stat_workflow(proteomes_output_folder, clustering_output_folder, annotation_output_folder, stat_file)
