b9f73b9d2d1ed76e6e120a9219fafb32591902b2
Arnaud Belcour <arnaud.belcour@gmail.com>
2024-09-30 16:38:53 +0200

    Update panel report creation.

--- esmecata/report/report_creation.py
+++ esmecata/report/report_creation.py
@@ -33,49 +33,47 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
     from bokeh.resources import INLINE, CDN
     import panel as pn
 
-    pn.extension("plotly")
-    pn.extension('mathjax')
-
     styles = {
         'background-color': '#F6F6F6', 'border': '2px solid black',
         'border-radius': '5px', 'padding': '10px'
     }
 
-    page_one = pn.Accordion(
+    page_one = pn.Column(
         pn.pane.Markdown("""# Reduction of taxonomic diversity between EsMeCaTa's inputs and outputs
-                    The sunburst below displays the taxonomic diversity of inputs, i.e. all the taxonomic affiliations listed in the tsv input file. White cells indicates inputs whose ranks were not retained by EsMeCaTa, which went up to the superior taxonomic rank to find proteomes.""", styles=styles),
+                    The sunburst below displays the taxonomic diversity of inputs, i.e. all the taxonomic affiliations listed in the tsv input file. White cells indicates inputs whose ranks were not retained by EsMeCaTa, which went up to the superior taxonomic rank to find proteomes.""",
+                    styles=styles, renderer='markdown'),
         pn.Column('## Sunburst of taxon selected by EsMeCaTa',
             pn.pane.Plotly(fig9.update_layout(autosize=True)),
-            sizing_mode='scale_height'),
+            sizing_mode='scale_both'),
 
-        pn.pane.HTML("""<h2>\"Compression\" of taxonomic diversity between inputs and outputs</h2>
-                    <p>The sankey diagram below also displays the initial taxonomic diversity of taxonomic affiliations given to EsMeCaTa as inputs. The first column details the content of the input file : one block is equivalent to one taxonomic affiliation, and the block height indicates the number of times this affiliation appears in the input file. The second column details which of these taxonomic affiliations were merged together (i.e. 'compressed') because their taxonomyies were identical in the input file. Finally, The first column displays the ranks EsMeCaTa attributed to each of them : at this step, some taxonomic affiliations can be merged because EsMeCaTa went up the taxonomic ranks to find proteomes.</p>""", styles=styles),
+        pn.pane.Markdown("""# \"Compression\" of taxonomic diversity between inputs and outputs
+                    The sankey diagram below also displays the initial taxonomic diversity of taxonomic affiliations given to EsMeCaTa as inputs. The first column details the content of the input file : one block is equivalent to one taxonomic affiliation, and the block height indicates the number of times this affiliation appears in the input file. The second column details which of these taxonomic affiliations were merged together (i.e. 'compressed') because their taxonomyies were identical in the input file. Finally, The first column displays the ranks EsMeCaTa attributed to each of them : at this step, some taxonomic affiliations can be merged because EsMeCaTa went up the taxonomic ranks to find proteomes.""", styles=styles),
         pn.Column('## Sankey diagram of taxonomic compression',
             pn.pane.Plotly(fig10.update_layout(autosize=True)),
-            sizing_mode='scale_height'),
+            sizing_mode='scale_both'),
     )
     page_two = pn.Column(
-        pn.pane.HTML("""<h2>Proteomes summary</h2>
-                     <p>When proteomes of the lowest taxonomic rank of an input taxa are not found, EsMeCaTa goes up in the taxonomic levels to found suited proteomes. In complement to the figures in the first panel, the heatmap below details the difference between the lowest taxonomic rank of all input taxonomic observations (y-axis) and the lowest rank atributed by EsMeCaTa (x-axis).</p>""", styles=styles),
+        pn.pane.Markdown("""# Proteomes summary
+                     When proteomes of the lowest taxonomic rank of an input taxa are not found, EsMeCaTa goes up in the taxonomic levels to found suited proteomes. In complement to the figures in the first panel, the heatmap below details the difference between the lowest taxonomic rank of all input taxonomic observations (y-axis) and the lowest rank atributed by EsMeCaTa (x-axis).""", styles=styles),
         pn.Column('## Input and output ranks difference',
             pn.pane.Plotly(fig4.update_layout(modebar=CONFIG, autosize=True)),
-            sizing_mode='scale_height'),
+            sizing_mode='scale_both'),
         pn.pane.HTML("""<p>The barplot belows details how many proteomes were assigned to inputs taxonomic affiliations (grouped and colored by phylum). The number of proteomes found can vary from only a few to dozens or hundreds, depending on the data available on UniProt for each taxonomic group.</p>""", styles=styles),
         pn.Column('## Distribution of the number of proteomes found',
             pn.pane.Plotly(fig1.update_layout(modebar=CONFIG, autosize=True)),
-            sizing_mode='scale_height'),
+            sizing_mode='scale_both'),
     )
     page_three = pn.Column(
             pn.pane.HTML("""<h2>Clustering summary</h2>
                          <p>In the clustering step, for the set of proteomes associated to each input taxa, EsMeCaTa searches for clusters of similar proteic sequences among these proteomes. This step is supervised by a threshold between 0 and 1, 1 meaning that a cluster contains sequences from each proteomes, 0.5 meaning that a cluster contains sequences from at least a half of the proteomes (etc). This threshold can then be an approximation of the pan-proteome (0) and the core-proteome (1). Clusters below the threshold are discarded. The figure below displays how many clusters were retained at each threshold value. The vertical red line displays the threshold set dor this run of EsMeCata.</p>""", styles=styles),
         pn.Column('## Proteomes representativeness',
             pn.pane.Plotly(fig12.update_layout(modebar=CONFIG, autosize=True)),
-            sizing_mode='scale_height'),
+            sizing_mode='scale_both'),
         pn.Column('## Proteomes representativeness details',
             pn.pane.Plotly(fig12_details.update_layout(modebar=CONFIG, autosize=True)),
-            sizing_mode='scale_width'),
+            sizing_mode='scale_both'),
     )
-    page_four = pn.Accordion(
+    page_four = pn.Column(
         # EC numbers figures
         pn.FlexBox(
             pn.pane.HTML("""<h2>EC numbers frequencies among taxa</h2>
@@ -84,13 +82,13 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
             pn.indicators.Number(name='EC found in less than 10% of taxa', value=DATA2["n_1_an"], format='{value}'),
             pn.indicators.Number(name='EC found between 10% and 90% of taxa', value=DATA2["n91_an"], format='{value}'),
         ),
-        pn.FlexBox(
+        pn.Column(
             pn.Column('## EC numbers frequencies in observations',
                 pn.pane.Plotly(fig5.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
             pn.Column('## EC numbers frequencies in observations (barplot version)',
                 pn.pane.Plotly(fig7.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
         ),
 
         pn.FlexBox(
@@ -100,19 +98,19 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
             pn.indicators.Number(name="Taxa with less than 10% of all the dataset's EC numbers", value=DATA2["n_1_ob"], format='{value}'),
             pn.indicators.Number(name="Taxa with between 10% and 90% of all the dataset's EC numbers", value=DATA2["n91_ob"], format='{value}'),
         ),
-        pn.FlexBox(
+        pn.Column(
             pn.Column('## Taxa content in EC numbers',
                 pn.pane.Plotly(fig6.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
             pn.Column('## Taxa content in EC numbers (barplot version)',
                 pn.pane.Plotly(fig8.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
         ),
 
         pn.pane.HTML("<h2>EC numbers classes, counts and proportions</h2>", styles=styles),
         pn.Column('## EC numbers classes, counts and proportions',
             pn.pane.Plotly(fig11.update_layout(modebar=CONFIG, autosize=True)),
-            sizing_mode='scale_height'),
+            sizing_mode='scale_both'),
 
         # GO terms figures
         pn.FlexBox(
@@ -122,13 +120,13 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
             pn.indicators.Number(name="GO terms found in less than 10% of taxa", value=DATA3["n_1_an"], format='{value}'),
             pn.indicators.Number(name="GO terms found between 10% and 90% of taxa", value=DATA3["n91_an"], format='{value}'),
         ),
-        pn.FlexBox(
+        pn.Column(
             pn.Column('## GO terms frequencies in taxa',
                 pn.pane.Plotly(fig5b.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
             pn.Column('## GO terms frequencies in taxa (barplot version)',
                 pn.pane.Plotly(fig7b.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
         ),
 
         pn.FlexBox(
@@ -138,14 +136,15 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
             pn.indicators.Number(name="Taxa with less than 10% of all the dataset's GO terms", value=DATA3["n_1_ob"], format='{value}'),
             pn.indicators.Number(name="Taxa with between 10% and 90% of all the dataset's GO terms", value=DATA3["n91_ob"], format='{value}'),
         ),
-        pn.FlexBox(
+        pn.Column(
             pn.Column("## Observations' content in GO terms",
                 pn.pane.Plotly(fig6b.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
             pn.Column("## Observations' content in GO terms (barplot version)",
                 pn.pane.Plotly(fig8b.update_layout(modebar=CONFIG, autosize=True)),
-                sizing_mode='scale_height'),
+                sizing_mode='scale_both'),
         ),
+        height=5000
     )
 
     if not DATA["DISCARDED"].empty:    
@@ -170,7 +169,7 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
     )
 
     page_six = pn.Column(
-        pn.pane.JSON(metadata, name="Metadata"),
+        pn.pane.JSON(metadata, name="Metadata", depth=-1),
     )
 
     tabs = pn.Tabs(("Inputs and outputs ranks comparison", page_one),
@@ -178,7 +177,8 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
                    ("Clustering summary", page_three),
                    ("Annotation summary", page_four),
                    ("Data summary", page_five),
-                   ("Metadata", page_six))
+                   ("Metadata", page_six),
+                   height_policy="fit", sizing_mode="fixed")
 
     panel = pn.template.VanillaTemplate(
         title="EsMeCaTa report",
@@ -186,7 +186,7 @@ def create_panel_report(CONFIG, DATA, DATA2, DATA3, fig1, fig4, fig5, fig5b, fig
         header_color='black',
         header_background='white'
     )
-
+ 
     if reduce is True:
         panel.save(output_file, resources=CDN)
     else:
