00b133b3ee91950e281960554268b1ea70ee1a13
Arnaud Belcour <arnaud.belcour@gmail.com>
2025-11-03 15:42:33 +0100

    Fix tests.

--- test/test_workflow_uniprot.py
+++ test/test_workflow_uniprot.py
@@ -8,7 +8,7 @@ from esmecata.core.precomputed import precomputed_parse_affiliation
 from esmecata.report.workflow_create_report import run_create_workflow_report
 
 RESULTS = {
-    'Cluster_1': {'proteomes': 2, 'protein_clusters': 603, 'GOs': 872, 'ECs': 314}
+    'Cluster_1': {'proteomes': 3, 'protein_clusters': 557, 'GOs': 831, 'ECs': 302}
 }
 
 def test_workflow_online():
@@ -28,7 +28,7 @@ def test_workflow_online():
             predicted_results[line[0]]['protein_clusters'] = int(line[7])
             predicted_results[line[0]]['GOs'] = int(line[8])
             predicted_results[line[0]]['ECs'] = int(line[9])
-
+    print(predicted_results)
     for observation_name in predicted_results:
         for data in predicted_results[observation_name]:
             assert predicted_results[observation_name][data] == RESULTS[observation_name][data]
